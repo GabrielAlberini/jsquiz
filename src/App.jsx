643445da5d7783d7e6b5e52d98de4d1c492d2f75
CuerpoCasa <gabialberini733@gmail.com>
2022-01-24 05:07:37 -0300

    V1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
+import {useState} from 'react'
 import {API} from './API'
 
 
 function App() {
+  const [questionCurrent, setQuestionCurrent] = useState(0)
+  const [points, setPoints] = useState(0)
+  const [isFinished, setIsFinished] = useState(false)
 
   return (
       <main className="app">
         <div className="lado-izquierdo">
           <div className="numero-pregunta">
-            <span>Pregunta 1 de </span>5 
+            <span>Pregunta {questionCurrent + 0} </span>de {API.length} 
           </div>
-          <div className="titulo-pregunta">¿Como te llamas?</div>
+          <div className="titulo-pregunta">{API[questionCurrent].titulo}</div>
         </div>
         <div className="lado-derecho">
+          {
+            API[questionCurrent].opciones.map((respuesta, i)=> <button key={i}>{respuesta.textoRespuesta}</button>)
+          }
           <button>Opcion 1</button>
-          <button>Opcion 2</button>
-          <button>Opcion 3</button>
-          <button>Opcion 4</button>
         </div>
       </main>
   )
